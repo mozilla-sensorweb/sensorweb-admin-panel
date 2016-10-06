85a0f2228680e1b31f1dbd585a45ccdeabd13996
Fernando Jiménez Moreno <ferjmoreno@gmail.com>
2016-10-06 12:39:11 +0200

    Show login form if user is not authenticated

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,26 @@
-import React from 'react';
-import NavigationBar from './NavigationBar';
-import FlashMessagesList from './flash/FlashMessagesList';
+import React              from 'react';
+import FlashMessagesList  from './flash/FlashMessagesList';
+import NavigationBar      from './NavigationBar';
+import SignIn             from './signin/SignIn';
+import { connect }        from 'react-redux';
 
 class App extends React.Component {
   render() {
+    const { isAuthenticated } = this.props.auth;
     return (
       <div className="container">
         <NavigationBar />
         <FlashMessagesList />
-        {this.props.children}
+        {(isAuthenticated ? this.props.children : <SignIn/>)}
       </div>
     );
   }
 }
 
-export default App;
+function mapStateToProps(state) {
+  return {
+    auth: state.auth
+  };
+}
+
+export default connect(mapStateToProps, {})(App);
